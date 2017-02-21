0523b50700ba3bd8c7c5968d8a2c7b8226164847
Jack Anderson <40208539@live.napier.ac.uk>
2017-02-21 15:45:21 +0000

    more changes from email to username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,6 +21,17 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
+     * Get the login username to be used by the controller.
+     * Override of username method from AuthenticatesUsers.php
+     *
+     * @return string
+     */
+    public function username()
+    {
+        return 'username';
+    }
+
+    /**
      * Where to redirect users after login.
      *
      * @var string
