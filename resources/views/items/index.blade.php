19f33b1882351be8a369a21de9d30641d3e3e069
Jack Anderson <40208539@live.napier.ac.uk>
2017-02-23 12:08:08 +0000

    responsive table to store item info

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -2,6 +2,50 @@
 
 @section('content')
 
-  <h1>Items page!</h1>
+  <div class="panel panel-default">
+    <div class="panel-heading">Items</div>
+    <div class="panel-body">
+    <div class="table-responsive">
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>Firstname</th>
+            <th>Lastname</th>
+            <th>Email</th>
+            <th>Email</th>
+            <th>Email</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>John</td>
+            <td>Doe</td>
+            <td>john@example.com</td>
+            <td>john@example.com</td>
+            <td>john@example.com</td>
+            <td>john@example.com</td>
+          </tr>
+          <tr>
+            <td>Mary</td>
+            <td>Moe</td>
+            <td>mary@example.com</td>
+            <td>john@example.com</td>
+            <td>john@example.com</td>
+            <td>john@example.com</td>
+          </tr>
+          <tr>
+            <td>July</td>
+            <td>Dooley</td>
+            <td>july@example.com</td>
+            <td>john@example.com</td>
+            <td>john@example.com</td>
+            <td>john@example.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    </div>
+  </div>
 
 @endsection
