51e3031425ac03763094c293d5f741b0372d4955
Jack Anderson <40208539@live.napier.ac.uk>
2017-03-01 22:32:48 +0000

    added spec form

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -5,8 +5,12 @@
     @include('includes.searchModal')
 
     <div class="panel panel-default">
-        <div class="panel-heading" data-toggle="collapse" data-target="#itemPanel">Item</div>
-        <div id="itemPanel" class="panel-body">
+        <div class="panel-heading tall-header">Item
+            <button type="button" class="btn btn-default pull-right" data-toggle="collapse" data-target="#itemPanel">
+                <span id="invCaret" class="fa fa-caret-down" aria-hidden="true"></span>
+            </button>
+        </div>
+        <div id="itemPanel" class="panel-body in">
             <form>
                 <div class="container-fluid">
                     <div class="row">
@@ -68,21 +72,114 @@
         </div>
     </div>
     <div class="panel panel-default">
-        <div class="panel-heading">Product Specification</div>
-        <div class="panel-body">
+        <div class="panel-heading tall-header">Product Specification
+            <button type="button" class="btn btn-default pull-right" data-toggle="collapse" data-target="#specPanel">
+                <span id="invCaret" class="fa fa-caret-down" aria-hidden="true"></span>
+            </button>
+        </div>
+        <div id="specPanel" class="panel-body in">
             <div class="row">
+              <form>
+                  <div class="container-fluid">
+                      <div class="row">
+                          <div class="form-group col-sm-12 col-md-6">
+                              <label for="brand" class="control-label">Brand</label>
+                              <input name="brand" class="form-control">
 
+                              </input>
+
+                              <label for="model" class="control-label">Model</label>
+                              <input name="model" class="form-control">
+
+                              </input>
+
+                              <label for="cpu" class="control-label">CPU</label>
+                              <input name="cpu" class="form-control">
+
+                              </input>
+
+                              <label for="ram" class="control-label">RAM</label>
+                              <input name="ram" class="form-control">
+
+                              </input>
+
+                              <label for="hdd" class="control-label">HDD</label>
+                              <input name="hdd" class="form-control">
+
+                              </input>
+
+                              <label for="odd" class="control-label">ODD</label>
+                              <input name="odd" class="form-control">
+
+                              </input>
+
+                              <label for="gpu" class="control-label">GPU</label>
+                              <input name="gpu" class="form-control">
+
+                              </input>
+
+                              <label for="battery" class="control-label">Battery</label>
+                              <input name="battery" class="form-control">
+
+                              </input>
+                          </div>
+
+
+                          <div class="form-group col-sm-12 col-md-6">
+                            <label for="usb" class="control-label">USB</label>
+                            <input name="usb" class="form-control">
+
+                            </input>
+
+                            <label for="lan" class="control-label">LAN</label>
+                            <input name="lan" class="form-control">
+
+                            </input>
+
+                            <label for="wlan" class="control-label">WLAN</label>
+                            <input name="wlan" class="form-control">
+
+                            </input>
+
+                            <label for="os" class="control-label">OS</label>
+                            <input name="os" class="form-control">
+
+                            </input>
+
+                            <label for="psu" class="control-label">PSU</label>
+                            <input name="psu" class="form-control">
+
+                            </input>
+
+                            <label for="screen_size" class="control-label">Screen Size</label>
+                            <input name="screen_size" class="form-control">
+
+                            </input>
+
+                            <label for="screen_rez" class="control-label">Screen Resolution</label>
+                            <input name="screen_rez" class="form-control">
+
+                            </input>
+
+                            <button class="btn btn-primary form-control form-btn" type="submit">Add Specification</button>
+                          </div>
+                      </div>
+                  </div>
+              </form>
             </div>
         </div>
     </div>
 
     <div class="panel panel-default">
         <div class="panel-heading tall-header">Inventory
-            <button type="button" class="btn btn-default search-button" data-toggle="modal" data-target="#searchModal">
+            <button type="button" class="btn btn-default pull-right" data-toggle="collapse" data-target="#invPanel">
+                <span id="invCaret" class="fa fa-caret-down" aria-hidden="true"></span>
+            </button>
+            <button type="button" class="btn btn-default pull-right" data-toggle="modal" data-target="#searchModal">
                 <span class="fa fa-search" aria-hidden="true"></span>
             </button>
         </div>
-        <div class="panel-body">
+        <div id="invPanel" class="panel-body in">
             <div class="table-responsive">
                 <table class="table table-striped">
                     <thead>
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
@@ -26,5 +25,4 @@
                 <a href="#" class="btn btn-primary pull-right">Create Member</a>
             </div>
         </div>
-    </div>
 @endsection
