9cea9db1137fafcba844af4fad20b6268212c155
Jack Anderson <40208539@live.napier.ac.uk>
2017-03-09 14:45:52 +0000

    disabled furniture for now, ADDED PRETTY CHARTS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,12 @@
+
+<input type="hidden" id="forsale" value="{{$counts['forsale']}}" />
+<input type="hidden" id="forparts" value="{{$counts['forparts']}}" />
+<input type="hidden" id="storage" value="{{$counts['storage']}}" />
+
 @extends('layouts.app')
 
 @section('content')
+
     <div class="row">
         <div class="col-sm-12">
             <div class="panel panel-default">
@@ -8,6 +14,8 @@
 
                 <div class="panel-body">
                     You are logged in!
+                    {{$counts['forsale']}}
+                    <canvas id="mycanvas"></canvas>
                 </div>
             </div>
         </div>
