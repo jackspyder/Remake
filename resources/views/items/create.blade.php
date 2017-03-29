b16a1b002488e1236ba930410d89ae90979a0df5
Jack Powell <jackspider@hotmail.com>
2017-03-29 03:12:02 +0100

    Create Item forms converted.
    Still need to bind submit and controller.

--- resources/views/items/create.blade.php
+++ resources/views/items/create.blade.php
@@ -137,102 +137,104 @@
     </div>
 
     <div class="panel panel-default">
-        <div class="panel-heading tall-header">Product Specification
+        {{--Panel head--}}
+        <div class="panel-heading tall-header">Add Specifications
+            {{--Panel minimise button--}}
             <button type="button" class="btn btn-default pull-right" data-toggle="collapse" data-target="#specPanel">
                 <span id="invCaret" class="fa fa-caret-down" aria-hidden="true"></span>
             </button>
         </div>
-        <div id="specPanel" class="panel-body in">
-            <div class="row">
-                <form role="form" method="POST" action="{{ url('/specs') }}">
-                    <div class="container-fluid">
-                        <div class="row">
-                            <div class="form-group col-sm-12 col-md-6">
-                                <label for="brand" class="control-label">Brand</label>
-                                <input name="brand" class="form-control" required>
 
-                                </input>
+        <div class="panel-body" id="specPanel">
 
-                                <label for="model" class="control-label">Model</label>
-                                <input name="model" class="form-control" required>
+            {{--Main Panel Content--}}
+            <div class="container-fluid">
+                <div class="row">
 
-                                </input>
+                    {{--Row Left--}}
+                    <div class="form-group col-sm-12 col-md-6">
+                        <div class="form-group">
+                            {{ Form::label('brand', 'Brand') }}
+                            {{ Form::text('brand', null, array('class' => 'form-control')) }}
+                        </div>
 
-                                <label for="cpu" class="control-label">CPU</label>
-                                <input name="cpu" class="form-control">
+                        <div class="form-group">
+                            {{ Form::label('model', 'Model') }}
+                            {{ Form::text('model', null, array('class' => 'form-control')) }}
+                        </div>
 
-                                </input>
+                        <div class="form-group">
+                            {{ Form::label('cpu', 'CPU') }}
+                            {{ Form::text('cpu', null, array('class' => 'form-control')) }}
+                        </div>
 
-                                <label for="ram" class="control-label">RAM</label>
-                                <input name="ram" class="form-control">
+                        <div class="form-group">
+                            {{ Form::label('ram', 'Memory (RAM)') }}
+                            {{ Form::text('ram', null, array('class' => 'form-control')) }}
+                        </div>
 
-                                </input>
+                        <div class="form-group">
+                            {{ Form::label('hdd', 'Hard Drive (HDD)') }}
+                            {{ Form::text('hdd', null, array('class' => 'form-control')) }}
+                        </div>
 
-                                <label for="hdd" class="control-label">HDD</label>
-                                <input name="hdd" class="form-control">
+                        <div class="form-group">
+                            {{ Form::label('odd', 'Optical Drive') }}
+                            {{ Form::text('odd', null, array('class' => 'form-control')) }}
+                        </div>
 
-                                </input>
+                        <div class="form-group">
+                            {{ Form::label('gpu', 'Graphics Card (GPU)') }}
+                            {{ Form::text('gpu', null, array('class' => 'form-control')) }}
+                        </div>
 
-                                <label for="odd" class="control-label">ODD</label>
-                                <input name="odd" class="form-control">
-
-                                </input>
-
-                                <label for="gpu" class="control-label">GPU</label>
-                                <input name="gpu" class="form-control">
-
-                                </input>
-
-                                <label for="battery" class="control-label">Battery</label>
-                                <input name="battery" class="form-control">
-
-                                </input>
-                            </div>
-
-
-                            <div class="form-group col-sm-12 col-md-6">
-                                <label for="usb" class="control-label">USB</label>
-                                <input name="usb" class="form-control">
-
-                                </input>
-
-                                <label for="lan" class="control-label">LAN</label>
-                                <input name="lan" class="form-control">
-
-                                </input>
-
-                                <label for="wlan" class="control-label">WLAN</label>
-                                <input name="wlan" class="form-control">
-
-                                </input>
-
-                                <label for="os" class="control-label">OS</label>
-                                <input name="os" class="form-control">
-
-                                </input>
-
-                                <label for="psu" class="control-label">PSU</label>
-                                <input name="psu" class="form-control">
-
-                                </input>
-
-                                <label for="screen_size" class="control-label">Screen Size</label>
-                                <input name="screen_size" class="form-control">
-
-                                </input>
-
-                                <label for="screen_rez" class="control-label">Screen Resolution</label>
-                                <input name="screen_rez" class="form-control">
-
-                                </input>
-
-                                <button class="btn btn-primary form-control form-btn" type="submit">Add Specification
-                                </button>
-                                <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                            </div>
+                        <div class="form-group">
+                            {{ Form::label('battery', 'Battery') }}
+                            {{ Form::text('battery', null, array('class' => 'form-control')) }}
                         </div>
                     </div>
-                </form>
+
+                    {{--Row Right--}}
+                    <div class="form-group col-sm-12 col-md-6">
+                        <div class="form-group">
+                            {{ Form::label('usb', 'USB Ports') }}
+                            {{ Form::text('usb', null, array('class' => 'form-control')) }}
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('lan', 'Network Port (LAN)') }}
+                            {{ Form::text('lan', null, array('class' => 'form-control')) }}
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('wlan', 'Wireless Card (WLAN)') }}
+                            {{ Form::text('wlan', null, array('class' => 'form-control')) }}
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('os', 'Operating System') }}
+                            {{ Form::text('os', null, array('class' => 'form-control')) }}
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('psu', 'Power Supply') }}
+                            {{ Form::text('psu', null, array('class' => 'form-control')) }}
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('screen_size', 'Screen Size') }}
+                            {{ Form::text('screen_size', null, array('class' => 'form-control')) }}
+                        </div>
+
+                        <div class="form-group">
+                            {{ Form::label('screen_rez', 'Screen Resolution') }}
+                            {{ Form::text('screen_rez', null, array('class' => 'form-control')) }}
+                        </div>
+                        <div class="form-group">
+                            {{ Form::submit('Add Spec', array('class' => 'btn btn-primary')) }}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
