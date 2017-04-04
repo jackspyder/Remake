accc1bc3559c77c3756c4dd95307dc04d2ec027a
Jack Powell <jackspider@hotmail.com>
2017-04-04 09:28:25 +0100

    branching test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
     <div class="row">
         <div class="col-sm-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Dashboard!</div>
 
                 <div class="panel-body">
                     <canvas id="mycanvas"></canvas>
