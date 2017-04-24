61280e0841f51490791685d55ce722c644ff9af4
Jack Powell <jackspider@hotmail.com>
2017-04-24 12:45:53 +0100

    initial customer controller functionality

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -37,7 +37,21 @@ class CustomersController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $this->validate(request(), [
+            'first_name' => 'nullable',
+            'last_name' => 'nullable',
+            'email' => 'unique:customers|required|email',
+            'phone_no' => 'unique'
+        ]);
+
+        $customer = new Customer;
+        $customer->first_name = request('first_name');
+        $customer->last_name = request('last_name');
+        $customer->email = request('email');
+        $customer->phone_no = request('phone_no');
+        $customer->save();
+
+        return redirect('/customers');
     }
 
     /**
@@ -48,7 +62,9 @@ class CustomersController extends Controller
      */
     public function show($id)
     {
-        //
+        $customer = Customer::withTrashed()->findOrFail($id);
+
+        return view('customers.show', compact('customer'));
     }
 
     /**
@@ -59,7 +75,9 @@ class CustomersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $customer = Customer::findOrFail($id);
+
+        return view('customers.edit', compact('customer'));
     }
 
     /**
@@ -71,7 +89,28 @@ class CustomersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $customer = Customer::findOrFail($id);
+
+        $this->validate(request(),[
+            'first_name' => 'nullable',
+            'last_name' => 'nullable',
+            'email' => [
+                'required',
+                Rule::unique('customers')->ignore($customer->email),
+            ],
+            'phone_no' => [
+                'required',
+                Rule::unique('customers')->ignore($customer->phone_no),
+            ]
+        ]);
+
+        $customer->first_name = request('first_name');
+        $customer->last_name = request('last_name');
+        $customer->email = request('email');
+        $customer->phone_no = request('phone_no');
+        $customer->save();
+
+        return redirect('/customers');
     }
 
     /**
